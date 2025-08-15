044ddd1a8ed111cc64ab7c85970547239fc0161e
kmartinezabarca <kmartinezabarca@users.noreply.github.com>
2025-08-14 21:17:38 -0400

    Initial commit: React frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// Import components for Admin and Client areas
+// import AdminLayout from './components/AdminLayout';
+// import ClientLayout from './components/ClientLayout';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+
+        {/* Client Routes */}
+        {/* <Route path="/client/*" element={<ClientLayout />} /> */}
+
+        {/* Admin Routes */}
+        {/* <Route path="/admin/*" element={<AdminLayout />} /> */}
+
+        {/* Default Route */}
+        <Route path="/" element={<div>Welcome to Hosting Platform!</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
